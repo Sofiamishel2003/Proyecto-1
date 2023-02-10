63a898cf33e1b1f95d3b7ce7cf669b7f7ff07cbd
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-09 21:27:45 -0600

    Update Conversor F a C.lisp

--- Proyecto 1/Fase 1/Conversor F a C.lisp
+++ Proyecto 1/Fase 1/Conversor F a C.lisp
@@ -1,13 +1,6 @@
-(defun fahrenheit-to-celsius (fahrenheit)
-  "Convert Fahrenheit to Celsius."
-  (/ (- fahrenheit 32) 1.8))
+(defun conversor (n)
+  (* (- n 32) 0.5555555556))
 
-(defun celsius-to-fahrenheit (celsius)
-  "Convert Celsius to Fahrenheit."
-  (* celsius 1.8 + 32))
-  
-  (fahrenheit-to-celsius 32)
-;; Resultado: 0.0
+(print (conversor 50))
+;; Resultado: 10
 
-(celsius-to-fahrenheit 100)
-;; Resultado: 212.0
